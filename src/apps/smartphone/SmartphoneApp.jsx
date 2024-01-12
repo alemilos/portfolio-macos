5052e35326ff2de900008b4cf732c82523979d3f
AleMilos <alessandro.milos@hotmail.it>
2024-01-12 16:36:28 +0100

    smartphone view underdev

--- src/apps/smartphone/SmartphoneApp.jsx
+++ src/apps/smartphone/SmartphoneApp.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+// Colors
+import { colors } from "../../colors";
+
+// Image
+import inDevelopment from "./assets/coding.png";
+
 const SmartphoneApp = () => {
-  return <div>smartphone app</div>;
+  return (
+    <div
+      style={{ backgroundColor: colors.notesDarkGrey }}
+      className="w-screen h-screen flex flex-col items-center gap-8 text-center px-4"
+    >
+      <p className="text-4xl font-bold text-white"> Under Development</p>
+      <p className="text-md text-white">
+        The Smartphone/Tablet View is currently under development.
+      </p>
+      <img src={inDevelopment} />
+    </div>
+  );
 };
 
 export default SmartphoneApp;
